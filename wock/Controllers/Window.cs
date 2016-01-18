fc4a92c388ac120a54bcaec75006cc36196ac427
Oskar Olsson <oskar.ols@gmail.com>
2016-01-18 14:06:44 -0800

    Initial commit

---
+++ wock/Controllers/Window.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Web;
+
+namespace wock.Controllers
+{
+    public class Window
+    {
+        public bool IsVisible;
+        public string Title;
+        private IntPtr windowHandler;
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        private static extern int GetWindowText(IntPtr hWnd, StringBuilder strText, int maxCount);
+
+        [DllImport("user32.dll", CharSet = CharSet.Unicode)]
+        private static extern int GetWindowTextLength(IntPtr hWnd);
+
+        [DllImport("user32.dll")]
+        protected static extern bool IsWindowVisible(IntPtr hWnd);
+    }
+}
---
+++ wock/Controllers/windows.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace wock.Controllers
+{
+    public class Windows
+    {
+
+        private List<Window> windows;
+
+        [DllImport("user32.dll")]
+        private static extern bool EnumWindows(EnumWindowsProc enumProc, IntPtr lParam);
+
+        // Delegate to filter which windows to include 
+        public delegate bool EnumWindowsProc(IntPtr hWnd, IntPtr lParam);
+
+
+        Windows()
+        {
+
+        }
+
+
+    }
+}
